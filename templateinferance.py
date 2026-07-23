a5c5a5014dce3ac0fb865a59aaf16ffdc068f282
Divya <eluminous.se64@gmail.com>
2026-07-23 18:57:04 +0530

    did changes for services html menu

--- templateinferance.py
+++ templateinferance.py
@@ -20,6 +20,9 @@ def profile():
         city="Nsk"
     )
 
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
